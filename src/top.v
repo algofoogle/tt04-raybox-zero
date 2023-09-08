dce37c8e7e93b47a569a16ca09aadeedac7871d0
Anton Maurovic <anton@foogle.com>
2023-09-09 04:07:22 +0930

    Update submodule for SPI2 (all other registers via 2nd SPI interface)

--- src/top.v
+++ src/top.v
@@ -27,30 +27,37 @@ module tt_um_algofoogle_raybox_zero(
   assign uo_out = i_reg ? registered_vga_output : unregistered_vga_output;
 
   rbzero rbzero(
-    .clk      (clk),
-    .reset    (~rst_n),
+    .clk        (clk),
+    .reset      (~rst_n),
 
     // SPI slave interface.
     //NOTE: These are internally synchronised.
-    .i_sclk   (ui_in[0]),
-    .i_mosi   (ui_in[1]),
-    .i_ss_n   (ui_in[2]),
+    .i_sclk     (ui_in[0]),
+    .i_mosi     (ui_in[1]),
+    .i_ss_n     (ui_in[2]),
 
     // Debug/demo signals:
     //SMELL: These are NOT internally synchronised. Should they be?
     // Since they're just for simple demo purposes, and not typically used, I think they're fine as-is:
-    .i_debug  (ui_in[3]),
-    .i_inc_px (ui_in[4]),
-    .i_inc_py (ui_in[5]),
+    .i_debug    (ui_in[3]),
+    .i_inc_px   (ui_in[4]),
+    .i_inc_py   (ui_in[5]),
 
     // VGA output signals:
-    .hsync_n  (hsync_n),
-    .vsync_n  (vsync_n),
-    .rgb      (rgb),
+    .hsync_n    (hsync_n),
+    .vsync_n    (vsync_n),
+    .rgb        (rgb),
 
     // Other outputs:
-    .o_hblank (uio_out[0]),
-    .o_vblank (uio_out[1])
+    .o_hblank   (uio_out[0]),
+    .o_vblank   (uio_out[1]),
+
+    // Alt SPI slave interface for all other general register access.
+    //NOTE: These are internally synchronised.
+    .i_reg_sclk (uio_in[2]),
+    .i_reg_mosi (uio_in[3]),
+    .i_reg_ss_n (uio_in[4])
+
   );
 
   assign uio_oe       = 8'b0000_0011; // 1 = output, 0 = input.
