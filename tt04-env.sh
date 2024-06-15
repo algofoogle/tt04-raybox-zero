35271ba92cf2a464a0b90bb4c80f85391411754f
Anton Maurovic <anton@foogle.com>
2024-06-15 14:31:02 +0930

    Trying to fix 'test' GHA

---
+++ tt04-env.sh
@@ -0,0 +1,7 @@
+export TTTOOLS=/home/anton/ttsetup@tt04
+export OPENLANE_ROOT=/home/anton/ttsetup@tt04/openlane
+export PDK_ROOT=/home/anton/ttsetup@tt04/pdk
+export PDK=sky130A
+export OPENLANE_TAG=2023.06.26
+export OPENLANE_IMAGE_NAME=efabless/openlane:3bc9d02d0b34ad032921553e512fbe4bebf1d833
+source /home/anton/ttsetup@tt04/venv/bin/activate
